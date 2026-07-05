7c363b1f9e945458ff6bcabf583fcb3b350ed030
James Lane <jlane75a@gmail.com>
2026-07-05 11:03:38 +0100

    Initial Commit

---
+++ HabitsTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HabitsTracker
+//
+//  Created by James Lane on 05/07/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
